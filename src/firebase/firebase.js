c26bab73d7a9660aff928d9548983b1fd854ccb3
Alex <alexandre.bornstein@sublimeskinz.com>
2018-06-01 18:08:29 +0200

    Learned Promises in JS, Remove, Update & Fetching Data with Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+import { constants } from 'os';
 
 const config = {
   apiKey: "AIzaSyBierOHDPm1szsMIaNdkkgc1tVZLAq7Q84",
@@ -11,6 +12,78 @@ const config = {
 
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  name: 'Alex'
+const database = firebase.database();
+
+database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(`${val.name} is ${val.job.title} at ${val.job.company}`);
 });
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, () => {
+//   console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//   database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(50);
+// }, 9500);
+
+
+
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//    const val = snapshot.val();
+//    console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', 0);
+//   });
+  
+// database.ref().set({
+//   name: 'Alex',
+//   age: 23,
+//   location: {
+//     city: 'Paris',
+//     state: 'France'
+//   },
+//   job: {
+//     title: 'Web Integrator',
+//     company: 'Sublime Skinz'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+
+// database.ref('age').remove()
+//   .then(() => {
+//     console.log('Data is removed');
+//   }).catch((e) => {
+//     console.log('This Failed.', e);
+//   });
+
+// database.ref().update({
+//   name: 'Peter',
+//   age: 19,
+//   'job/title': 'Front-End Dev',
+//   'job/company': 'Altima',
+//   isSingle: false,
+//   'location/city': 'Lyon'
+// }).then(() => {
+//   console.log('Data has been Updated');
+// }).catch((e) => {
+//   console.log('Update Failed.', e);
+// });
