7697a93cbed346f8c1deb647448a28167f7b5011
Alex <alexandre.bornstein@sublimeskinz.com>
2018-06-20 18:02:08 +0200

    Creating a Separate Test Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import { constants } from 'os';
 
 const config = {
-  apiKey: "AIzaSyBierOHDPm1szsMIaNdkkgc1tVZLAq7Q84",
-  authDomain: "expensify-ddb2f.firebaseapp.com",
-  databaseURL: "https://expensify-ddb2f.firebaseio.com",
-  projectId: "expensify-ddb2f",
-  storageBucket: "expensify-ddb2f.appspot.com",
-  messagingSenderId: "1024498394465"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -9,7 +9,11 @@ console.log('before');
 
 promise.then((data) => {
   console.log('1', data);
-  return 'some data';
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+    }, 1500);
+  });
 }).then(() => {
   console.log('does this run?', str);
 }).catch((error) => {
